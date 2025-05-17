f57d7f49d0ccde00119edd81ee7ba303dc70dfb3
Ritika Vasuja <ritikavsj@gmail.com>
2025-05-17 14:55:21 +0530

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 
@@ -22,6 +24,8 @@ app.UseCors(x => x
     .WithOrigins("http://localhost:4200",
         "https://localhost:4200"
         ));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
